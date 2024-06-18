67776a449c603d315dfee76d4ee1183221bd815c
Allan Kipkorir <allantuix@gmail.com>
2024-06-18 13:33:55 +0300

    completed sushi sga App

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import SushiContainer from "./SushiContainer";
 import Table from "./Table";
 
 const API = "http://localhost:3001/sushis";
 
 function App() {
+  const [sushis, setSushis] = useState([]);
+  const [eatenSushis, setEatenSushis] = useState([]);
+  const [money, setMoney] = useState(100);
+
+  useEffect(() => {
+    fetch(API)
+      .then(response => response.json())
+      .then(data => setSushis(data));
+  }, []);
+
+  const eatSushi = (sushi) => {
+    if (money >= sushi.price) {
+      setEatenSushis([...eatenSushis, sushi]);
+      setMoney(money - sushi.price);
+    } else {
+      alert("Not enough money!");
+    }
+  };
+
   return (
     <div className="app">
-      <SushiContainer />
-      <Table />
+      <SushiContainer sushis={sushis} eatenSushis={eatenSushis} eatSushi={eatSushi} />
+      <Table plates={eatenSushis} money={money} />
     </div>
   );
 }
 
 export default App;
+
--- src/components/SushiContainer.js
+++ src/components/SushiContainer.js
@@ -1,13 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import Sushi from "./Sushi";
 import MoreButton from "./MoreButton";
 
-function SushiContainer(props) {
+function SushiContainer({ sushis, eatenSushis, eatSushi }) {
+  const [startIndex, setStartIndex] = useState(0);
+
+  const displayedSushis = sushis.slice(startIndex, startIndex + 4);
+
+  const handleMoreSushi = () => {
+    setStartIndex((startIndex + 4) % sushis.length);
+  };
+
   return (
     <div className="belt">
-      {/* Render Sushi components here! */}
-      <MoreButton />
+      {displayedSushis.map(sushi => (
+        <Sushi
+          key={sushi.id}
+          sushi={sushi}
+          isEaten={eatenSushis.includes(sushi)}
+          eatSushi={eatSushi}
+        />
+      ))}
+      <MoreButton onClick={handleMoreSushi} />
     </div>
   );
 }
 
 export default SushiContainer;
+
